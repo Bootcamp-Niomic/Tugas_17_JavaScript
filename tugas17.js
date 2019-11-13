d2e0d831e12f37859d7436cc0a0f30f5838874a1
Havid <great_havid@yahoo.co.id>
2019-11-13 11:43:24 +0700

    Tugas 17 JS

---
+++ tugas17.js
@@ -0,0 +1,9 @@
+// ====== Tugas 17 JavaScript =====
+
+var data = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+console.log(data.toLowerCase());
+console.log(data.toUpperCase());
+
+var data2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(data2.toLowerCase());
+console.log(data2.toUpperCase());
